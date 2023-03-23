8fa56e2233e7b3adcc754fef6c9c59d2cb4f3bb9
amanuelmandefro2 <128055581+amanuelmandefro2@users.noreply.github.com>
2023-03-23 06:16:42 +0300

    Create bubble-sort.py
    bubble sort problem solution for hackerrank problems

---
+++ bubble-sort.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+
+def countSwaps(a):
+    numSwaps = 0
+    for i in range(len(a)):
+        lst = tuple(a);
+        for j in range(len(a) - 1):
+            if(a[j] > a[j +1]):
+                temp = a[j]
+                a[j] = a[j +1]
+                a[j + 1] = temp
+                numSwaps += 1
+                        
+    firstElement = a[0]
+    lastElement = a[len(a) - 1]
+    print(f'Array is sorted in {numSwaps} swaps.')
+    print(f'First Element: {firstElement}')
+    print(f'Last Element: {lastElement}')
+     
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)
